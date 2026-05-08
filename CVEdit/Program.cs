d0bf821ec00fb8bb68a00affd4c194bffb5bc914
emils-crypto <emils@vesama.com>
2026-05-08 13:55:40 +0300

    Add project files.

---
+++ CVEdit/Program.cs
@@ -0,0 +1,30 @@
+﻿using SelectPdf;
+
+var htmlPath = Path.GetFullPath("../../../CV.html");
+var html = File.ReadAllText(htmlPath);
+
+var dir = Path.GetDirectoryName(htmlPath)!;
+var baseUrl = new Uri(dir + Path.DirectorySeparatorChar).AbsoluteUri;
+
+var converter = new HtmlToPdf();
+
+converter.Options.PdfPageSize = PdfPageSize.A4;
+converter.Options.PdfPageOrientation = PdfPageOrientation.Portrait;
+
+converter.Options.MarginTop = 0;
+converter.Options.MarginBottom = 0;
+converter.Options.MarginLeft = 0;
+converter.Options.MarginRight = 0;
+
+converter.Options.WebPageFixedSize = true;
+converter.Options.WebPageWidth = 794;
+converter.Options.WebPageHeight = 1118; 
+
+converter.Options.AutoFitHeight = HtmlToPdfPageFitMode.NoAdjustment;
+converter.Options.AutoFitWidth = HtmlToPdfPageFitMode.NoAdjustment;
+
+converter.Options.DenyLocalFileAccess = false;
+
+var doc = converter.ConvertHtmlString(html, baseUrl);
+doc.Save("../../../CV.pdf");
+doc.Close();
